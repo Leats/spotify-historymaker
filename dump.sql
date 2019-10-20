a6cb47b2590fbce5abe01c4ea17c48e86763c504
leats <Lea.tschiersch@fu-berlin.de>
2019-10-20 11:39:07 +0200

    Add database schema and requirements.txt A schema-only db_dump was added so it is understandable how the database is built. Requirements now have their own file.

---
+++ dump.sql
@@ -0,0 +1,182 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.5
+-- Dumped by pg_dump version 11.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: album; Type: TABLE; Schema: public; Owner: spotify
+--
+
+CREATE TABLE public.album (
+    title character varying NOT NULL,
+    uri character varying NOT NULL,
+    artisturi character varying,
+    id character varying NOT NULL
+);
+
+
+ALTER TABLE public.album OWNER TO spotify;
+
+--
+-- Name: artist; Type: TABLE; Schema: public; Owner: spotify
+--
+
+CREATE TABLE public.artist (
+    name character varying NOT NULL,
+    uri character varying NOT NULL,
+    id character varying NOT NULL
+);
+
+
+ALTER TABLE public.artist OWNER TO spotify;
+
+--
+-- Name: context; Type: TABLE; Schema: public; Owner: spotify
+--
+
+CREATE TABLE public.context (
+    uri character varying NOT NULL,
+    type character varying NOT NULL
+);
+
+
+ALTER TABLE public.context OWNER TO spotify;
+
+--
+-- Name: cursors; Type: TABLE; Schema: public; Owner: spotify
+--
+
+CREATE TABLE public.cursors (
+    after character varying NOT NULL,
+    before character varying
+);
+
+
+ALTER TABLE public.cursors OWNER TO spotify;
+
+--
+-- Name: played; Type: TABLE; Schema: public; Owner: spotify
+--
+
+CREATE TABLE public.played (
+    trackuri character varying NOT NULL,
+    artisturi character varying,
+    contexturi character varying,
+    "time" timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.played OWNER TO spotify;
+
+--
+-- Name: track; Type: TABLE; Schema: public; Owner: spotify
+--
+
+CREATE TABLE public.track (
+    title character varying NOT NULL,
+    uri character varying NOT NULL,
+    artisturi character varying,
+    albumuri character varying,
+    id character varying NOT NULL,
+    duration integer,
+    explicit boolean,
+    popularity smallint,
+    tracknumber smallint
+);
+
+
+ALTER TABLE public.track OWNER TO spotify;
+
+--
+-- Name: album album_pkey; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.album
+    ADD CONSTRAINT album_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: album album_uri_key; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.album
+    ADD CONSTRAINT album_uri_key UNIQUE (uri);
+
+
+--
+-- Name: artist artist_pkey; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.artist
+    ADD CONSTRAINT artist_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: artist artist_uri_key; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.artist
+    ADD CONSTRAINT artist_uri_key UNIQUE (uri);
+
+
+--
+-- Name: context context_pkey; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.context
+    ADD CONSTRAINT context_pkey PRIMARY KEY (uri);
+
+
+--
+-- Name: cursors cursors_pkey; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.cursors
+    ADD CONSTRAINT cursors_pkey PRIMARY KEY (after);
+
+
+--
+-- Name: played played_pkey; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.played
+    ADD CONSTRAINT played_pkey PRIMARY KEY ("time");
+
+
+--
+-- Name: track track_pkey; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.track
+    ADD CONSTRAINT track_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: track track_uri_key; Type: CONSTRAINT; Schema: public; Owner: spotify
+--
+
+ALTER TABLE ONLY public.track
+    ADD CONSTRAINT track_uri_key UNIQUE (uri);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
